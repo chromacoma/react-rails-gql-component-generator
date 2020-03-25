95f6ae82dd952c4a97ac07c4c5442c3f6427b139
johna <john@minml.net>
2020-03-25 10:58:46 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+// const styles = require("./styles.js");
+
+if (process.argv.length < 3) {
+  console.error("Component name should be provided");
+  process.exit(1);
+}
+
+const componentPath = process.argv[process.argv.length - 1];
+const conponentParts = componentPath.split("/");
+const componentName = conponentParts.pop();
+const dir = path.join(process.cwd(), ...conponentParts);
+
+if (fs.existsSync(dir)) {
+  console.error("Sorry, component", componentPath, "already exists. Exiting...");
+  process.exit(1);
+} else {
+  fs.mkdirSync(dir);
+}
+
+function writeToFile(name, getData = () => "") {
+  const pathToFile = path.join(dir, name);
+  const componentName = path.basename(dir);
+  fs.writeFile(path.join(dir, name), getData(componentName), { encoding: "utf8" }, err => {
+    if (err) {
+      console.error("Error writing file!", err);
+      process.exit(1);
+    } else {
+      console.log("Creating", pathToFile);
+    }
+  });
+}
+
+const toLower = str => `${str.charAt(0).toLowerCase()}${str.slice(1)}`;
+
+writeToFile("operations.gql");
+writeToFile("index.js", componentName => {
+  return `import React from 'react';
+import styes from './styles';
+const ${componentName} = () => (
+  <div className={styes.${toLower(componentName)}}>👋</div>
+);
+export default ${componentName};`;
+});
+writeToFile("styles.module.scss", componentName => {
+  return `.${toLower(componentName)} {
+
+}`;
+});
